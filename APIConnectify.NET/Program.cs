ed6c44315705c6df33b87c54343f72dc9c7af7b5
Maksim9056 <bobreczovm@inbox.ru>
2024-04-09 13:47:33 +0300

    Правки

--- APIConnectify.NET/Program.cs
+++ APIConnectify.NET/Program.cs
@@ -26,8 +26,8 @@ namespace APIConnectify.NET
 
             //builder.Services.AddDbContext<DB>(options =>
             //   options.UseNpgsql(builder.Configuration.GetConnectionString("APIConnectifyNETContext") ?? throw new InvalidOperationException("Connection string 'APIConnectifyNETContext' not found.")));
-            builder.Services.AddDbContext<DB>(options =>
-               options.UseNpgsql(builder.Configuration.GetConnectionString("APIConnectifyNETContext") ?? throw new InvalidOperationException("Connection string 'APIConnectifyNETContext' not found.")));
+         //   builder.Services.AddDbContext<DB>(options =>
+         //      options.UseNpgsql(builder.Configuration.GetConnectionString("APIConnectifyNETContext") ?? throw new InvalidOperationException("Connection string 'APIConnectifyNETContext' not found.")));
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll",
