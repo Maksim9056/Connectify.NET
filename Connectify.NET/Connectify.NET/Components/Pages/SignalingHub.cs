bb49444f45135ea4fd335dd7714b0f599a3ffb10
Maks <bobreczovm@inbox.ru>
2024-04-19 23:47:41 +0300

    Правки

---
+++ Connectify.NET/Connectify.NET/Components/Pages/SignalingHub.cs
@@ -0,0 +1,39 @@
+﻿namespace Connectify.NET.Components.Pages
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class SignalingHub : Hub
+    {
+        public async Task JoinAudioRoom(string roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Clients.OthersInGroup(roomId).SendAsync("UserJoined", Context.ConnectionId);
+        }
+
+        public async Task SendAudio(string roomId, byte[] audioData)
+        {
+            await Clients.OthersInGroup(roomId).SendAsync("ReceiveAudio", audioData);
+        }
+
+        public async Task LeaveAudioRoom(string roomId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+            await Clients.OthersInGroup(roomId).SendAsync("UserLeft", Context.ConnectionId);
+        }
+
+        public async Task StartCall()
+        {
+            // Отправить сигнал о начале звонка другим участникам
+            await Clients.Others.SendAsync("ReceiveCallStart");
+        }
+
+        public async Task EndCall()
+        {
+            // Отправить сигнал об окончании звонка другим участникам
+            await Clients.Others.SendAsync("ReceiveCallEnd");
+        }
+    }
+
+
+}
--- Connectify.NET/Connectify.NET/Program.cs
+++ Connectify.NET/Connectify.NET/Program.cs
@@ -1,4 +1,5 @@
 using Connectify.NET.Components;
+using Connectify.NET.Components.Pages;
 using Microsoft.AspNetCore.Hosting;
 using System.ComponentModel;
 
@@ -9,61 +10,68 @@ namespace Connectify.NET
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            {
 
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
+                // Add services to the container.
+                builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents();
-            builder.Services.AddHttpClient();
-            builder.Services.AddCors();
-            builder.Services.AddScoped<Connectify.NET.Components.Pages.Component>();
-            builder.Services.AddScoped<Connectify.NET.Components.Pages.GroupAdd>();
-            builder.Services.AddScoped<Connectify.NET.Components.Pages.GroupChats>();
-            builder.Services.AddScoped<Connectify.NET.Components.Pages.ListChatsAll>();
-            builder.Services.AddScoped<Connectify.NET.Components.Pages.Friends>();
-            builder.Services.AddScoped<Component>();
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                               .AllowAnyMethod()
-                               .AllowAnyHeader().WithHeaders();
-                    });
-            });
+                builder.Services.AddHttpClient();
+                builder.Services.AddCors();
+                builder.Services.AddScoped<Connectify.NET.Components.Pages.Component>();
+                builder.Services.AddScoped<Connectify.NET.Components.Pages.GroupAdd>();
+                builder.Services.AddScoped<Connectify.NET.Components.Pages.GroupChats>();
+                builder.Services.AddScoped<Connectify.NET.Components.Pages.ListChatsAll>();
+                builder.Services.AddScoped<Connectify.NET.Components.Pages.Friends>();
+                builder.Services.AddScoped<System.ComponentModel.Component>();
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("AllowAll",
+                        builder =>
+                        {
+                            builder.AllowAnyOrigin()
+                                   .AllowAnyMethod()
+                                   .AllowAnyHeader().WithHeaders();
+                        });
+                });
+                builder.Services.AddRazorPages();
+                builder.Services.AddSignalR();
+                builder.Services.AddServerSideBlazor();
+                var app = builder.Build();
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseWebAssemblyDebugging();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseRouting();
 
-            builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
-            var app = builder.Build();
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
+                app.UseStaticFiles();
+
+                app.UseHttpsRedirection();
+
+                app.UseAntiforgery();
+
+                app.UseCors("AllowAll");
+
+                app.MapRazorComponents<App>()
+                    .AddInteractiveServerRenderMode()
+                    .AddInteractiveWebAssemblyRenderMode()
+                    .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                    endpoints.MapHub<SignalingHub>("/signalr");
+                });
+
+                app.Run();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseStaticFiles();
-
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.UseCors("AllowAll");
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode()
-                .AddInteractiveWebAssemblyRenderMode()
-                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
-
-            var port = 5200;
-        
-
-            app.Run();
         }
     }
 }
